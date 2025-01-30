59f8b7379e76f0a89fc4cab74dedcac7764832a9
Chang Ha Lee <yielding@gmail.com>
2025-01-30 19:44:01 +0900

    .

--- binary_tree/main.cpp
+++ binary_tree/main.cpp
@@ -1,55 +1,108 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <list>
+#include <algorithm>
 
 using namespace std;
 
 struct Node
 {
-    Node(const string& name)
-        : _name(name)
+    Node(const int& no)
+        : _no(no)
     {
-        left_child = right_child = nullptr;
+        left = right = nullptr;
     }
 
-    string _name;
-    Node* left_child;
-    Node* right_child;
+    int _no;
+    Node* left;
+    Node* right;
 };
 
 class Binary_tree
 {
 public:
-    Binary_tree()
+    Binary_tree(Node* in)
     {
-
+        _root = in;
     }
 
-    void visit(const Node* in)
+    template <typename Visitor>
+    auto visit(Visitor& visitor, int options) -> void
     {
-        if (in != nullptr)
-            cout << in->_name << endl;
+        if (options == 0) preorder(_root, visitor);
+        if (options == 1) inorder(_root, visitor);
+        if (options == 2) postorder(_root, visitor);
     }
 
-    void preorder(const Node* in)
+private:
+    template <typename Visitor>
+    void preorder(const Node* in, Visitor& visitor)
     {
-        if (in != nullptr)
-        {
-            visit(in);
-            preorder(in->left_child);
-            preorder(in->right_child);
-        }
+        if (in == nullptr) return;
+
+        visitor(in);
+        preorder(in->left,  visitor);
+        preorder(in->right, visitor);
+    }
+
+    template <typename Visitor>
+    void inorder(const Node* in, Visitor& visitor)
+    {
+        if (in == nullptr) return;
+
+        inorder(in->left,  visitor);
+        visitor(in);
+        inorder(in->right, visitor);
+    }
+
+    template <typename Visitor>
+    void postorder(const Node* in, Visitor& visitor)
+    {
+        if (in == nullptr) return;
+
+        postorder(in->left,  visitor);
+        postorder(in->right, visitor);
+        visitor(in);
     }
 
     // 노드 삽입 함수
-    
 
 private:
-
+    Node* _root;
 };
-
 
 int main()
 {
+    Node* n = new Node(0);     // _root == 0
+    n->left = new Node(1);     // 1
+    n->right = new Node(2);    // 2 
+                               //
+    Node* one = n->left;
+    one->left = new Node(3);   // 3
+    one->right = new Node(4);  // 4
+
+    Node* two = n->right;
+    two->left = new Node(5);    // 5
+    two->right = new Node(6);   // 6
+
+    Binary_tree bt(n);
+
+    int sum = 0;
+    auto summer = [&sum](const auto node) { sum += node->_no; };
+    
+    bt.visit(summer, 0);
+
+    cout << sum << endl;
+
+    // 1. lambda
+    // 2. tree traversal
+    // 3. template programming
+
+    vector<int> v = { 10, 1, 2, 3, 4, 5 };
+    list<int> l = { 10, 1, 2, 3, 4, 5 };
+
+    sort(l.begin(), l.end(), [](const int& a, const int& b) { return a < b; });
 
     return 0;
 }
