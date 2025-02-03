63e03d6bcc7575c0dbe2ec8f027841b0ccc4246a
LawFu1l <gunhui0511@naver.com>
2025-02-03 19:14:36 +0900

    hashtable added

---
+++ data.structure/hashtable/main.cpp
@@ -0,0 +1,188 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/*
+ *  올려야하는 식별자의 수, 버킷에 올라간 식별자의 수, 총 버킷의 수, 입력 데이터 타입, 식별자 밀집도
+ *
+ */
+
+struct Node
+{
+    Node(const string& name)
+        : _name{name}, _next{nullptr}
+    {}
+
+    Node() = default;
+
+    string _name;
+    Node* _next;
+};
+
+enum class Option {All, Table, Metadata};
+
+class Hashtable
+{
+public:
+    Hashtable(const int& bucket_size)
+        : _bucket_size(bucket_size)
+    {
+        _bucket.resize(_bucket_size);
+    }
+
+    void insert(vector<string>& input)
+    {
+        for (auto& s : input)
+            insert(s);
+    }
+
+    void remove(const string& name)
+    {
+        int position = djb2(name);
+        auto unremoved = _bucket[position];
+        auto tmp = new Node();
+
+        if (unremoved->_name == name)
+        {
+            tmp = unremoved->_next;
+            delete unremoved;
+            _bucket[position] = tmp;
+        }
+        else
+        {
+            auto tmp = _bucket[position];
+            while (tmp->_next->_name != name)
+            {
+                tmp = tmp->_next;
+            }
+            if (tmp->_next == nullptr) delete tmp;
+            else
+            {
+                auto cur = tmp;
+                tmp = tmp->_next->_next;
+                delete cur->_next;
+                cur->_next = tmp;
+            }
+        }
+    }
+
+    //해시 테이블의 상태값을 보여주고, 해시 테이블 전체를 보여준다.
+    void view_status(Option option)
+    {
+        switch (option)
+        {
+            case Option::All:
+                cout << "---------------------------------" << endl;
+                cout << "VIEW MODE : ALL" << endl;
+                view_all();
+                break;
+
+            case Option::Table:
+                cout << "---------------------------------" << endl;
+                cout << "VIEW MODE : TABLE" << endl;
+                view_table();
+                break;
+
+            case Option::Metadata:
+                cout << "VIEW MODE : Metadata" << endl;
+                view_metadata();
+                break;
+
+            default:
+                cout << "Undefined option for view_status() " << endl;
+                break;
+        }
+    }
+
+    ~Hashtable()
+    {
+
+    }
+// private function
+private:
+
+    void viewmode_util(string& mode)
+    {
+        cout << "---------------------------------" << endl;
+        printf("VIEW MODE : %s", mode);
+    }
+
+    void view_all()
+    {
+        view_metadata();
+        view_table();
+    }
+    
+    void view_table()
+    {
+
+        for (int i = 0; i < _bucket_size; i++)
+        {
+            if (_bucket[i] == nullptr) cout << "empty";
+            else
+            {
+                cout << _bucket[i]->_name << " ";
+                auto tmp = _bucket[i];
+
+                while (tmp->_next != nullptr)
+                {
+                    cout << tmp->_next->_name << " ";
+                    tmp = tmp->_next;
+                }
+            }
+            cout << endl;
+        }
+    }
+    void view_metadata()
+    {
+        cout << "---------------------------------" << endl;
+    }
+
+    void insert(const string& name)
+    {
+        int position = djb2(name);
+        auto tmp = new Node(name);
+        // 버킷이 비어있는 경우
+        if (_bucket[position] == nullptr)
+        {
+            _bucket[position] = tmp;
+        }
+        else
+        {
+            //버킷이 이미 노드를 가진 경우
+            auto n = _bucket[position];
+            while (n->_next != nullptr)
+            {
+                n = n->_next;
+            }
+            n->_next = tmp;
+        }
+    }
+
+    // 이 해시 테이블에서 사용할 해시 함수
+    unsigned int djb2(const string key)
+    {
+        unsigned long hash = 5381;
+        for (char c : key) {
+            hash = ((hash << 5) + hash) + c;  // hash * 33 + c
+        }
+        return hash % this->_bucket_size;
+    }
+
+private:
+    vector<Node*> _bucket;
+    int _bucket_size;
+};
+
+int main()
+{
+    Hashtable table(5);
+    vector<string> month{"jan", "feb", "mar", "apr", "may", "jun","jul", "aug", "sep", "oct", "nov", "dec"};
+    table.insert(month);
+    table.view_status(Option::Table);
+    table.remove("jan");
+
+    return 0;
+}
