c207e1b9f9a92a8c810881c55543735e43a26758
Gun Hee Lee <gunhui@naver.com>
2025-03-07 18:12:50 +0900

    fibonacci number python ver

---
+++ fibonacci_py/fib.py
@@ -0,0 +1,18 @@
+# fib (n) 은 배열의 n번째 인덱스를 반환한다.배열의 크기는 n + 1
+
+def fib(n):
+    num_list = [1, 1]
+        # 3을 받았다고 치자.
+    if n < 0:
+        raise IndexError("음수 인덱스 불가")
+    if n < 2:
+        return num_list[n]
+
+    for i in range(2, n+1):
+        num_list.append(num_list[i-1] + num_list[i -2])
+
+    return num_list[i]
+
+a = int(input())
+result = fib(a)
+print(result)
